d2d275873ea30c40d87400bc24570f77a768346d
Tecter <742054040@qq.com>
2017-05-12 17:13:18 +0800

    Update settings.py

--- myblog/settings.py
+++ myblog/settings.py
@@ -104,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hans'  # 默认en-us
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'  # 默认UTC
 
 USE_I18N = True
 
